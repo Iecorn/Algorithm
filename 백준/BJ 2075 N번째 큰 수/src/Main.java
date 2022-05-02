8552b8cb1f61abed925d9000cb87c1a93c88974d
ECON <lgyoon621@naver.com>
2022-05-02 21:59:41 +0900

    2022.05.02 백준 2075 N번째 큰 수

---
+++ 백준/BJ 2075 N번째 큰 수/src/Main.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+        int N = Integer.parseInt(br.readLine());
+        int count = 0;
+
+        int[] numbers = new int[N*N];
+
+        for(int i = 0; i < N; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < N; j++){
+                numbers[count++] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        Arrays.sort(numbers);
+
+        System.out.println(numbers[N*N-N]);
+    }
+
+}
