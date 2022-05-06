37a28460e02c42e1748456c33debb6d9f4d785c5
ECON <lgyoon621@naver.com>
2022-05-06 21:27:40 +0900

    2022.05.06 프로그래머스 다음 큰 숫자

---
+++ 프로그래머스/PG 다음 큰 숫자/src/Solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int ones = 0;
+
+        ones = Integer.bitCount(n);
+
+        int i = 1;
+        int count = 0;
+
+        while(true){
+            count = Integer.bitCount(n+i);
+
+            if(ones == count) break;
+
+            i++;
+        }
+
+        answer = n+i;
+
+        return answer;
+    }
+}
+
