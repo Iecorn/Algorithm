d92349d1d2bd9dd704342cc3312b0b07d3638c53
ECON <lgyoon621@naver.com>
2022-05-07 14:42:09 +0900

    2022.05.07 프로그래머스 점프와 순간 이동

--- 프로그래머스/PG 다음 큰 숫자/src/Solution.java
+++ 프로그래머스/PG 다음 큰 숫자/src/Solution.java
@@ -1,18 +1,14 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        int ones = 0;
-
-        ones = Integer.bitCount(n);
+        int ones = Integer.bitCount(n);
 
         int i = 1;
         int count = 0;
 
         while(true){
             count = Integer.bitCount(n+i);
-
             if(ones == count) break;
-
             i++;
         }
 
---
+++ 프로그래머스/Pycharm/PG 점프와 순간 이동.py
@@ -0,0 +1,17 @@
+
+
+def solution(n):
+    ans = 0
+    tmp = int(n)
+
+    while tmp > 0:
+        if tmp % 2 == 0:
+            tmp = tmp/2
+        else:
+            tmp -= 1
+            ans += 1
+
+    return ans
+
+
+print(solution(5000))
