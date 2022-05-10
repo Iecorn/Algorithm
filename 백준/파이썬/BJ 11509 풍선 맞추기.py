2bb12e28f6c338be1dd6a4019181ad4d431b0740
ECON <lgyoon621@naver.com>
2022-05-10 23:06:32 +0900

    2022.05.10 백준 11509 풍선 맞추기

---
+++ 백준/파이썬/BJ 11509 풍선 맞추기.py
@@ -0,0 +1,15 @@
+import sys
+
+N = int(sys.stdin.readline())
+balloons = list(map(int, sys.stdin.readline().split()))
+flag = [0]*(1000001)
+
+for i in balloons:
+    if flag[i] > 0:
+        flag[i] -= 1
+        flag[i-1] += 1
+    else :
+        flag[i-1] += 1
+
+print(sum(flag))
+
