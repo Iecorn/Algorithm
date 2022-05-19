013dbe92b66a10266f37e7a29b57e49781f1620f
ECON <lgyoon621@naver.com>
2022-05-19 14:48:22 +0900

    2022.05.19 백준 6198 옥상 정원 꾸미기

---
+++ 백준/BJ_6198_Bad_Hair_Day/src/Main.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+                new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] bulidings = new int[N];
+        long answer = 0;
+
+        for(int i = 0; i < N; i++)
+            bulidings[i] = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < N-1; i++){
+            for(int j = i+1; j < N; j++){
+                if (bulidings[j] < bulidings[i])
+                    answer++;
+                else
+                    break;
+            }
+        }
+
+        System.out.println(answer);
+
+    }
+
+}
