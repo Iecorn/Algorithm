16c79134c2ca48273c5f20daefdfb278a663b207
ECON <lgyoon621@naver.com>
2022-05-21 00:33:22 +0900

    2022.05.20 프로그래머스 주식가격

---
+++ 프로그래머스/Pycharm/PG 주식가격.py
@@ -0,0 +1,13 @@
+from typing import List
+
+def solution(prices:List[int]) -> List[int] :
+    answer = [i for i in range(len(prices) - 1 , -1, -1)]
+    stack = [0]
+
+    for i in range(1,len(prices)):
+        while stack and prices[stack[-1]] > prices[i]:
+            j = stack.pop()
+            answer[j] = i - j
+        stack.append(i)
+
+    return answer
