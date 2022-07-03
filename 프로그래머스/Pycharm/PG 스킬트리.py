a63a7d3a0830b655f7c9eb27a18bbe97c4e5167a
ECON <lgyoon621@naver.com>
2022-07-03 14:11:25 +0900

    2022.07.03 프로그래머스 스킬트리

---
+++ 프로그래머스/Pycharm/PG 스킬트리.py
@@ -0,0 +1,23 @@
+from typing import List
+
+def solution(skill : str, skill_trees : List[str]):
+    answer = 0
+    index = 0
+
+
+    for s in skill_trees:
+        flag = True
+        index = 0
+        for c in s:
+            if c in skill and index < len(skill) and skill[index] == c:
+                index += 1
+                continue;
+            elif c in skill and index < len(skill) and skill[index] != c:
+                flag = False
+                break;
+        if flag:
+            answer += 1
+
+    return answer
+
+print(solution("CBD", ["BACDE", "CBADF", "AECB", "BDA"]))
