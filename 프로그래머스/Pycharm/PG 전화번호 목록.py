ca4d95eecd41fdb326a2e9d49a2a7766d932a2c4
ECON <lgyoon621@naver.com>
2022-07-14 21:37:56 +0900

    2022.07.14 프로그래머스 전화번호 목록

---
+++ 프로그래머스/Pycharm/PG 전화번호 목록.py
@@ -0,0 +1,11 @@
+from typing import List
+
+def solution(phoneBook):
+    phoneBook = sorted(phoneBook)
+
+    for p1, p2 in zip(phoneBook, phoneBook[1:]):
+        if p2.startswith(p1):
+            return False
+    return True
+
+print(solution(["12","123","1235","567","88"]))
